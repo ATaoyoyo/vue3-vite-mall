288d04e9ba097c11cf16a65a54930948fe08c9d5
ATaoyoyo <1029187998@qq.com>
2021-05-11 23:13:00 +0800

    feat: 新增Header组件
    新增Header组件

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <Header />
+    <Swiper :list="swiperList" />
+  </div>
 </template>
 
 <script>
+import Header from '/view/Header'
+import Swiper from '/cpn/Swiper'
+import { onMounted, reactive, toRefs } from 'vue'
+
+import { getHomeRequest } from '../../api/home'
+
 export default {
   name: 'home',
 
-  components: {},
+  components: { Header, Swiper },
 
   setup() {
+    const state = reactive({
+      swiperList: [],
+      recommends: [],
+    })
 
-  }
+    onMounted(async () => {
+      const { data } = await getHomeRequest()
+      const { carousels, hotGoodses, newGoodses, recommendGoodses } = data
+
+      state.swiperList = carousels.map((item) => {
+        return {
+          id: item.carouselUrl,
+          src: item.carouselUrl,
+          url: item.redirectUrl,
+        }
+      })
+
+      state.recommends = recommendGoodses
+
+      console.log(carousels, hotGoodses, newGoodses, recommendGoodses)
+    })
+
+    return { ...toRefs(state) }
+  },
 }
 </script>
 
 <style lang="less" scoped>
 .home {
-
 }
 </style>
---
+++ src/view/Header/Header.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="Header">
+    <router-link tag="i" class="Header__menu" to="../../page/category">
+      <van-icon name="wap-nav"></van-icon>
+    </router-link>
+    <div class="Header__search">
+      <span class="Header__search__app-name">新蜂商城</span>
+      <i class="divider" />
+      <router-link tag="span" class="Header__search__title" to="/">
+        山河无恙，人间皆安
+      </router-link>
+    </div>
+    <router-link tag="a" class="Header__login" to="../../page/login"
+      ><span>登陆</span></router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  components: {},
+
+  setup() {},
+}
+</script>
+
+<style lang="less" scoped>
+.Header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 0;
+  height: 35px;
+  background: #1baeae;
+
+  &__menu {
+    display: flex;
+    align-items: center;
+    padding: 0 5px;
+    font-size: 26px;
+    font-weight: bold;
+    color: #fff;
+  }
+
+  &__search {
+    flex: 1;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 0 10px;
+    padding: 0 10px;
+    border-radius: 20px;
+    background: rgba(255, 255, 255, 0.7);
+
+    .divider {
+      width: 1px;
+      height: 15px;
+      background: #333;
+    }
+
+    &__app-name {
+      font-size: 20px;
+      font-weight: bold;
+      color: #1baeae;
+    }
+
+    &__title {
+      font-size: 12px;
+      font-weight: 400;
+      color: #666;
+    }
+  }
+
+  &__login {
+    padding: 0 5px;
+    font-size: 13px;
+    color: #fff;
+  }
+}
+</style>
