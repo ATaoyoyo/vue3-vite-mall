4becd34741f5137c6fa39cdfd51e0c31c6d1b0c4
ATaoyoyo <1029187998@qq.com>
2021-05-11 23:09:05 +0800

    feat: 增加category，login路由
    增加category，login路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router'
 
+const Home = () => import('/@/page/home')
+const Category = () => import('/@/page/category')
+const Login = () => import('/@/page/login')
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/',
-      redirect: '/home'
+      redirect: '/home',
     },
     {
       path: '/home',
       name: 'home',
       meta: { index: 1 },
-      component: () => import(/* webpackChunkName: "home" */ '/@/page/home')
-    }
-  ]
+      component: Home,
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: Category,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+  ],
 })
 
 export default router
-
