5cd2f31db709ad02f2f14237382f42b9c55dfd67
ATaoyoyo <1029187998@qq.com>
2021-05-11 23:12:26 +0800

    feat: 新增swipe组件
    新增swipe组件

---
+++ src/components/Swiper/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <van-swipe class="Swiper" :autoplay="3000" indicator-color="white">
+    <van-swipe-item v-for="item in list" :key="item.id">
+      <img :src="item.src" alt="" @click="handToPage(item.url)" />
+    </van-swipe-item>
+  </van-swipe>
+</template>
+
+<script>
+export default {
+  name: 'Swiper',
+
+  props: {
+    list: {
+      type: Array,
+      default: [],
+      isRequire: true,
+    },
+  },
+
+  components: {},
+
+  setup() {},
+
+  methods: {
+    handToPage(url) {
+      window.open(url)
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.Swiper {
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
