7d937bd2382aa7be704e3e53d912950854b292c9
ATaoyoyo <1029187998@qq.com>
2021-05-11 23:09:43 +0800

    feat: 新增home数据接口
    新增home数据接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,32 @@
-import axios from "axios";
+import axios from 'axios'
+
+const BASE_URL =
+  process.env.NODE_ENV == 'development'
+    ? '//backend-api-01.newbee.ltd/api/v1'
+    : '//backend-api-01.newbee.ltd/api/v1'
 
 const request = axios.create({
-  baseURL: 'http://backend-api-01.newbee.ltd',
-  timeout: 5000
+  baseURL: BASE_URL,
+  timeout: 5000,
 })
 
+request.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
 
-request.interceptors.request.use(config => {
-  return config
-}, err => {
-  return Promise.reject(err)
-})
-
-
-request.interceptors.response.use(res => {
-  return res
-}, err => {
-  return Promise.reject(err)
-})
+request.interceptors.response.use(
+  (res) => {
+    const { data } = res
+    return data
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
 
 export default request
