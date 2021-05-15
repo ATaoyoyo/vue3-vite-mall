1f1f0091730b594a52cb291bea9992266405234a
ATaoyoyo <1029187998@qq.com>
2021-05-15 15:26:38 +0800

    feat: axios实例封装更改

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { Toast } from 'vant'
+import router from '../router'
+
+import { getLocal } from '../utils'
 
 const BASE_URL =
   process.env.NODE_ENV == 'development'
@@ -8,6 +12,14 @@ const BASE_URL =
 const request = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
+  withCredentials: true,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    token: getLocal('token') || '',
+    post: {
+      'Content-Type': 'application/json',
+    },
+  },
 })
 
 request.interceptors.request.use(
@@ -21,7 +33,24 @@ request.interceptors.request.use(
 
 request.interceptors.response.use(
   (res) => {
-    const { data } = res
+    const {
+      data,
+      data: { resultCode, message },
+    } = res
+
+    if (typeof data !== 'object') {
+      Toast.fail('服务端异常！')
+      return Promise.reject(res)
+    }
+
+    if (resultCode !== 200) {
+      if (message) Toast.fail(message)
+
+      if (resultCode === 416) router.push({ name: 'login' })
+
+      return Promise.reject(data)
+    }
+
     return data
   },
   (err) => {
