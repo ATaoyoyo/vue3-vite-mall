024c54a8ef4346dd22fc06d92db159dc5ce2d645
ATaoyoyo <1029187998@qq.com>
2021-07-20 16:43:57 +0800

    简化ts组合式useStore用法

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,16 @@
-import {createStore, Store} from "vuex";
-import { InjectionKey } from 'vue'
+import {createStore, Store, useStore as baseUseStore} from "vuex";
+import {InjectionKey} from 'vue'
 
 import state from "./state";
 import actions from "./actions";
 import mutations from "./mutations";
+import {State} from "src/types/State";
 
-export const key:InjectionKey<Store<State>> = Symbol()
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export function useStore() {
+  return baseUseStore(key)
+}
 
 export default createStore({
   state,
